d97639092d3105ceab1d6f7273f3612d3cdc06b6
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-04-27 16:56:28 +0530

    Create Print 1 in whole array.cpp

---
+++ Print 1 in whole array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[10]={1,3,4,5,5};
+   for(int i=0;i<4;i++){
+    arr[i]=1;
+   }
+   for(int i=0;i<4;i++){
+    cout<<arr[i];
+   }
+}
